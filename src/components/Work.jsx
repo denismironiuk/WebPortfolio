20b16d0931ab003b43f1db7539fd027b1e66936e
denismironiuk <denis.mironik@gmail.com>
2023-01-24 18:32:42 +0200

    add projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import LazyLoad from "react-lazy-load";
+import { Link } from "react-scroll";
+import phonebook from '../assets/projects/phonebook.png'
+import netflix from '../assets/projects/netflix.png'
 
 function Work() {
   return (
@@ -14,7 +17,7 @@ function Work() {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 shadow-lg shadow-indigo-500/50 ">
           <div
             style={{
-              backgroundImage: `url(https://images.pexels.com/photos/313690/pexels-photo-313690.jpeg?auto=compress&cs=tinysrgb&w=1600)`,
+              backgroundImage: `url(${phonebook})`,
             }}
             className="shadow-lg shadow-[#040c16 group container rounded-md flex justify-center items-center mx-auto content-div"
           >
@@ -22,14 +25,18 @@ function Work() {
               <span></span>
               <div>
                 <a className="p-4" href="/">
+                  <a href="https://phonebook-complete.herokuapp.com/contacts">
                   <button className="text-center rounded-lg px-4 py-3 bg-white text-[lightblue] font-bold text-lg bg-black/60 ">
                     Demo
                   </button>
+                  </a>
                 </a>
                 <a className="p-4" href="/">
+                  <a href="https://github.com/denismironiuk/Phonebook">
                   <button className="text-center rounded-lg px-4 py-3 bg-white text-[lightblue] font-bold text-lg  bg-black/60">
                     Code{" "}
                   </button>
+                  </a>
                 </a>
               </div>
             </div>
@@ -37,22 +44,26 @@ function Work() {
 
           <div
             style={{
-              backgroundImage: `url(https://images.pexels.com/photos/313690/pexels-photo-313690.jpeg?auto=compress&cs=tinysrgb&w=1600)`,
+              backgroundImage: `url(${netflix})`,
             }}
             className="shadow-lg shadow-[#040c16 group container rounded-md flex justify-center items-center mx-auto content-div"
           >
-            <div>
+             <div>
               <span></span>
               <div>
                 <a className="p-4" href="/">
+                  <a href="https://test-b367c.web.app/login">
                   <button className="text-center rounded-lg px-4 py-3 bg-white text-[lightblue] font-bold text-lg bg-black/60 ">
                     Demo
                   </button>
+                  </a>
                 </a>
                 <a className="p-4" href="/">
+                  <a href="https://github.com/denismironiuk/Netflix">
                   <button className="text-center rounded-lg px-4 py-3 bg-white text-[lightblue] font-bold text-lg  bg-black/60">
                     Code{" "}
                   </button>
+                  </a>
                 </a>
               </div>
             </div>
